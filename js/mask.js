dc749a01bb20e00e2fecfec98ab36a39f06a9de1
Lee-sojung <thwjd3698@gmail.com>
2022-01-04 09:58:32 +0900

    전개연산자로 옵션 객체 합치기

--- js/mask.js
+++ js/mask.js
@@ -1,25 +1,36 @@
 class Mask {
-    constructor() {
-        this.init();
+
+    constructor(opt) {
+        const defaults= {
+            frame: ".wrap",
+            btns: "#btns li",
+            panels: "section div",
+            class_names: {on: "active", lower:"lower", mask:"mask"}
+        }
+
+        const result = {...defaults, ...opt};
+
+        this.init(result);
         this.bindingEvent();
     }
 
-    init() {
-        this.wrap = document.querySelector("main");
-        this.btns = this.wrap.querySelectorAll("#navi li");
-        this.panels = this.wrap.querySelectorAll("section article");
+    init(result) {
+        this.wrap = document.querySelector(result.frame);
+        this.btns = this.wrap.querySelectorAll(result.btns);
+        this.panels = this.wrap.querySelectorAll(result.panels);
         this.isOn = null;
         this.enableClick = true;
+        this.class_names = result.class_names;
 
         for(let panels of this.panels){
-            panels.classList.remove("mask");
+            panels.classList.remove(this.class_names.mask);
         }
     }
 
     bindingEvent() {
         this.btns.forEach((btn, index) => {
             btn.addEventListener("click", e => {
-                this.isOn = e.currentTarget.classList.contains("on");
+                this.isOn = e.currentTarget.classList.contains(this.class_names.on);
                 if(this.isOn) return;
 
                 if(this.enableClick){
@@ -33,32 +44,32 @@ class Mask {
     activation(index) {
         for (let i = 0; i < this.btns.length; i++) {
             //모든 버튼을 비활성화
-            this.btns[i].classList.remove("on");
+            this.btns[i].classList.remove(this.class_names.on);
 
             //기존활성화 되어있는 패널에만 mask클래스를 붙여서 모션 시작
-            if (this.panels[i].classList.contains("on")) {
-                this.panels[i].classList.add("mask");
+            if (this.panels[i].classList.contains(this.class_names.on)) {
+                this.panels[i].classList.add(this.class_names.mask);
             }
         }
 
         //현재 클릭한 순번에 해당하는 버튼만 활성화
-        this.btns[index].classList.add("on");
+        this.btns[index].classList.add(this.class_names.on);
 
         //현재 클릭한 순번에 해당하는 패널에 lower를 붙여서 바로 아래쪽에 위치시킴
-        this.panels[index].classList.add("lower");
+        this.panels[index].classList.add(this.class_names.lower);
 
         setTimeout(() => {
             for (let i = 0; i < this.panels.length; i++) {
                 //기존에 on으로 활성화 되어있던 패널만 찾아서 on, mask클래스 제거
-                if (this.panels[i].classList.contains("on")) {
-                    this.panels[i].classList.remove("on");
-                    this.panels[i].classList.remove("mask");
+                if (this.panels[i].classList.contains(this.class_names.on)) {
+                    this.panels[i].classList.remove(this.class_names.on);
+                    this.panels[i].classList.remove(this.class_names.mask);
                 }
             }
 
             //클릭한 순번인 패널만 lower클래스를 지우고 on클래스를 붙여서 화면 제일 최상단에 배치
-            this.panels[index].classList.remove("lower");
-            this.panels[index].classList.add("on");
+            this.panels[index].classList.remove(this.class_names.lower);
+            this.panels[index].classList.add(this.class_names.on);
 
             setTimeout(()=>{
                 this.enableClick = true;
